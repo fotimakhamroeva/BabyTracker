f0d8d2f896ddaf1d7e0f390befa7d8e016843cb0
mhakbar <habibakbar@Live.com>
2022-03-24 10:04:30 -0700

    Add files via upload
    Updated tables

---
+++ 01_users.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS parent CASCADE;
+DROP TABLE IF EXISTS baby CASCADE;
+DROP TABLE IF EXISTS parent_baby CASCADE;
+DROP TABLE IF EXISTS log CASCADE;
+
+
+CREATE TABLE parent (
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255),
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+);
+
+
+CREATE TABLE baby (
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255),
+  date_of_birth TIMESTAMP,
+  born_at VARCHAR(255)
+  picture_url,
+
+)
+
+CREATE TABLE parent_baby (
+  id SERIAL PRIMARY KEY NOT NULL,
+  FOREIGN KEY (baby_id) REFERENCES baby(id),
+  FOREIGN KEY (created_by) REFERENCES parents(id),
+
+)
+
+CREATE TABLE log (
+  id SERIAL PRIMARY KEY NOT NULL,
+  event_type VARCHAR(255) NOT NULL,
+  event_detail VARCHAR NOT NULL,
+  event_datetime DATE,
+  FOREIGN KEY (baby_id) REFERENCES baby(id),
+  FOREIGN KEY (created_by) REFERENCES parents(id),
+)
+
+
+
+
