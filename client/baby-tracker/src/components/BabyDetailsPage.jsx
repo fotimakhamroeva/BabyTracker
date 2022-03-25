01aa6f75e330abaac276eb2b0d325e4637458549
Fotima Khamroeva <bonu_9229@bk.ru>
2022-03-25 16:55:42 -0400

    update babydetail page sections

--- client/baby-tracker/src/components/BabyDetailsPage.jsx
+++ client/baby-tracker/src/components/BabyDetailsPage.jsx
@@ -6,6 +6,7 @@ import { UserContext } from "../context/userContext";
 import LogList from "./LogList";
 import BabyInfo from "./BabyInfo";
 import "./BabyDetailsPage.scss"
+import Button from "./Button";
 
 
 export default function BabyDetailsPage(props) {
@@ -26,10 +27,34 @@ export default function BabyDetailsPage(props) {
   return(
     <div className="row align-items-start">
       <section className="col">
-        <BabyInfo babyName={babyDetails.first_name} dateOfBirth={babyDetails.date_of_birth} birthLocation={babyDetails.birth_location} babyPic={babyDetails.picture_url} />
+        <BabyInfo babyName={babyDetails.first_name + " " + babyDetails.last_name} dateOfBirth={babyDetails.date_of_birth} birthLocation={babyDetails.birth_location} babyPic={babyDetails.picture_url} />
         <div className="detailBabyInfoSpace" />
         <section className="section">
-          Log Options here
+          <h4 className="mb-3">Health</h4>
+          <div className="row">
+            <div class="col d-flex justify-content-start">
+              <Button confirm children="Log Temperature" />
+              <Button confirm children="Log Temperature" />
+            </div>
+          </div>
+          <div className="row">
+            <div class="col d-flex justify-content-start">
+              <Button confirm children="Log Temperature" />
+              <Button confirm children="Log Temperature" />
+            </div>
+          </div>
+          <h4 className="mt-4 mb-3">Growth</h4>
+          <div className="row">
+            <div class="col d-flex justify-content-start">
+              <Button confirm children="Log Temperature" />
+              <Button confirm children="Log Temperature" />
+              <Button confirm children="Log Temperature" />
+            </div>
+          </div>
+          <h4 className="mt-4 mb-3">Charts</h4>
+          <div className="row">
+            <h6>Chart here</h6>
+          </div>
         </section>
         <div className="detailBabyInfoSpace" />
       </section>
