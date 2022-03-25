9c10873f30ee96fe03115802fe9ada2e163977b4
Fotima Khamroeva <bonu_9229@bk.ru>
2022-03-25 00:15:06 -0400

    updated BabyDetailsPage with resizable sections

--- client/baby-tracker/src/components/BabyDetailsPage.jsx
+++ client/baby-tracker/src/components/BabyDetailsPage.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../context/userContext";
 import LogList from "./LogList";
+import BabyInfo from "./BabyInfo";
 import "./BabyDetailsPage.scss"
 
 
@@ -15,10 +16,15 @@ export default function BabyDetailsPage(props) {
 
   return(
     <div className="row align-items-start">
-      <section className="section col">
-        Test
+      <section className="col">
+        <BabyInfo babyName="Bob" dateOfBirth="Jan 1st, 2022" birthLocation="Home" babyPic="https://cdn-icons-png.flaticon.com/512/191/191526.png" />
+        <div className="detailBabyInfoSpace" />
+        <section className="section">
+          Log Options here
+        </section>
+        <div className="detailBabyInfoSpace" />
       </section>
-      <section className="detailMoreSection">
+      <section className="col">
         <LogList />
       </section>
     </div>
