8f7f877fb3c2f0ba7be80e92480253463927c7a5
Fotima Khamroeva <bonu_9229@bk.ru>
2022-03-26 16:57:57 -0400

    changed class to className

--- client/baby-tracker/src/components/LogEventModal.jsx
+++ client/baby-tracker/src/components/LogEventModal.jsx
@@ -11,19 +11,19 @@ export default function LogEventModal(props) {
 
    return(
       <form onSubmit={(e) => e.preventDefault()}>
-         <div class="modal fade" id={props.modalId} data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog">
-               <div class="modal-content">
-                  <div class="modal-header">
-                     <h5 class="modal-title">{props.modalTitle}</h5>
-                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+         <div className="modal fade" id={props.modalId} data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div className="modal-dialog">
+               <div className="modal-content">
+                  <div className="modal-header">
+                     <h5 className="modal-title">{props.modalTitle}</h5>
+                     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
-                  <div class="modal-body">
-                     <div class="row g-3 align-items-center">
-                        <div class="col-3">
-                           <label for="title" class="col-form-label">Title</label>
+                  <div className="modal-body">
+                     <div className="row g-3 align-items-center">
+                        <div className="col-3">
+                           <label for="title" className="col-form-label">Title</label>
                         </div>
-                        <div class="col-5">
+                        <div className="col-5">
                            <input 
                               type="text" 
                               className="form-control" 
@@ -35,11 +35,11 @@ export default function LogEventModal(props) {
                               />
                         </div>
                      </div>
-                     <div class="row g-3 align-items-center mt-1">
-                        <div class="col-3">
-                           <label for="detail" class="col-form-label">Detail</label>
+                     <div className="row g-3 align-items-center mt-1">
+                        <div className="col-3">
+                           <label for="detail" className="col-form-label">Detail</label>
                         </div>
-                        <div class="col-5">
+                        <div className="col-5">
                            <input 
                               type="text" 
                               className="form-control" 
@@ -51,21 +51,21 @@ export default function LogEventModal(props) {
                               />
                         </div>
                      </div>
-                     <div class="row g-3 align-items-center mt-1">
-                        <div class="col-3">
-                           <label for="datepicker" class="col-form-label">Date</label>
+                     <div className="row g-3 align-items-center mt-1">
+                        <div className="col-3">
+                           <label for="datepicker" className="col-form-label">Date</label>
                         </div>
-                        <div class="col-5">
+                        <div className="col-5">
                            <div id="datepicker">
                               <DatePicker date={new Date()} />
                            </div>
                         </div>
                      </div>
-                     <div class="row g-3 align-items-center mt-1">
-                        <div class="col-3">
-                           <label for="timepicker" class="col-form-label">Time</label>
+                     <div className="row g-3 align-items-center mt-1">
+                        <div className="col-3">
+                           <label for="timepicker" className="col-form-label">Time</label>
                         </div>
-                        <div class="col-5">
+                        <div className="col-5">
                            <div id="timepicker">
                               <TimePicker date={new Date()} />
                            </div>
@@ -73,9 +73,9 @@ export default function LogEventModal(props) {
                      </div>
                     
                   </div>
-                  <div class="modal-footer">
-                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                     <button type="button" class="btn btn-primary">Submit</button>
+                  <div className="modal-footer">
+                     <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                     <button type="button" className="btn btn-primary">Submit</button>
                   </div>
                </div>
             </div>
