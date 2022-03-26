d16ed9116b01a39a9b97f0bf5e6e0bfaac5ee4fc
mhakbar <habibakbar@live.com>
2022-03-26 08:32:30 -0700

    working schema updated as per the routes

--- server/db/schema/01_users.sql
+++ server/db/schema/01_users.sql
@@ -4,41 +4,39 @@ DROP TABLE IF EXISTS parent_baby CASCADE;
 DROP TABLE IF EXISTS log CASCADE;
 
 
-CREATE TABLE parent (
+CREATE TABLE parent(
   id SERIAL PRIMARY KEY NOT NULL,
   first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255),
   email VARCHAR(255) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
 
 
-CREATE TABLE baby (
+CREATE TABLE baby(
   id SERIAL PRIMARY KEY NOT NULL,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255),
   date_of_birth TIMESTAMP,
-  born_at VARCHAR(255)
-  picture_url,
-
-)
+  born_at VARCHAR(255),
+  picture_url VARCHAR(255)
+);
 
 CREATE TABLE parent_baby (
   id SERIAL PRIMARY KEY NOT NULL,
-  FOREIGN KEY (baby_id) REFERENCES baby(id),
-  FOREIGN KEY (created_by) REFERENCES parents(id),
-
-)
+  babyID INT REFERENCES baby(id),
+  createdBY INT REFERENCES parent(id)
+);
 
 CREATE TABLE log (
   id SERIAL PRIMARY KEY NOT NULL,
   event_type VARCHAR(255) NOT NULL,
-  event_detail VARCHAR NOT NULL,
+  event_amount VARCHAR NOT NULL,
+  event_unit VARCHAR NOT NULL,
   event_datetime DATE,
-  FOREIGN KEY (baby_id) REFERENCES baby(id),
-  FOREIGN KEY (created_by) REFERENCES parents(id),
-)
-
+  babyID INT REFERENCES baby(id),
+  createdBY INT REFERENCES parent(id)
+);
 
 
 
