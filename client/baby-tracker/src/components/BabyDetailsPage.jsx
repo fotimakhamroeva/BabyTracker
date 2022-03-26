ecd5cfa8015b73b7542bbcab53ecc5830771f9b5
Fotima Khamroeva <bonu_9229@bk.ru>
2022-03-25 20:40:23 -0400

    fixed the loghistory to show logs by the baby id

--- client/baby-tracker/src/components/BabyDetailsPage.jsx
+++ client/baby-tracker/src/components/BabyDetailsPage.jsx
@@ -59,7 +59,7 @@ export default function BabyDetailsPage(props) {
         <div className="detailBabyInfoSpace" />
       </section>
       <section className="col">
-        <LogList />
+        <LogList babyId={id} />
       </section>
     </div>
   )
